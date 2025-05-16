0f2ccf82859e91a2eb26a3442d162c9c7f5501f7
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-16 07:38:58 +0000

    Convert MediaPlayerHelper to Kotlin

--- app/src/main/java/ai/elimu/keyboard/util/MediaPlayerHelper.kt
+++ app/src/main/java/ai/elimu/keyboard/util/MediaPlayerHelper.kt
@@ -1,28 +1,28 @@
-package ai.elimu.keyboard.util;
+package ai.elimu.keyboard.util
 
-import android.content.Context;
-import android.media.MediaPlayer;
-import android.util.Log;
+import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
+import android.util.Log
 
 /**
- * Utility class which helps releasing the {@link MediaPlayer} instance after
+ * Utility class which helps releasing the [MediaPlayer] instance after
  * finishing playing the audio.
- * <p />
+ *
+ *
  *
  * See https://developer.android.com/reference/android/media/MediaPlayer.html#create%28android.content.Context,%20int%29
  */
-public class MediaPlayerHelper {
+object MediaPlayerHelper {
+    fun play(context: Context?, resId: Int) {
+        Log.i(MediaPlayerHelper::class.java.getName(), "play")
 
-    public static void play(Context context, int resId) {
-        Log.i(MediaPlayerHelper.class.getName(), "play");
-
-        final MediaPlayer mediaPlayer = MediaPlayer.create(context, resId);
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mediaPlayer.release();
+        val mediaPlayer = MediaPlayer.create(context, resId)
+        mediaPlayer.setOnCompletionListener(object : OnCompletionListener {
+            override fun onCompletion(mp: MediaPlayer?) {
+                mediaPlayer.release()
             }
-        });
-        mediaPlayer.start();
+        })
+        mediaPlayer.start()
     }
 }
